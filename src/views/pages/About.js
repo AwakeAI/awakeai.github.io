66c965f20f1dd5587360815c9e972013d3559f92
linsh <lin.shilei@outlook.com>
2023-07-02 14:26:56 -0400

    Add Product svgs

--- src/views/pages/About.js
+++ src/views/pages/About.js
@@ -1,31 +1,41 @@
 // ** Reactstrap Imports
-import { Button } from "reactstrap";
+import {Button, Col, Row} from "reactstrap";
 
 // ** Custom Components
 import { Settings, Shield, Target } from "react-feather";
 import older from "@src/assets/images/pages/older.jpg";
+import frame from "@src/assets/images/pages/frame.svg";
+import framecn from "@src/assets/images/pages/frame-cn.svg";
 import { useTranslation } from "react-i18next";
 
 // ** Source Code
 const About = () => {
-  const { t } = useTranslation();
+  const { t, i18n  } = useTranslation();
 
   return (
-    <div id="about" className="sec-2" style={{ zIndex: "-1" }}>
+    <div id="about" className="sec-2" style={{ zIndex: "1" }}>
       <div className="container mx-auto">
         <div className="sec-2H1">{t("OPS")}</div>
-        <div className="sec-2H2">{t("CA")}</div>
+        <div className="sec-2H2">{t("Product")}</div>
+        <img
+          className="sec-3L mx-auto" style={{marginTop:"-5%", width:"70%"}}
+          src={`${i18n.language}`==='cn'?`${framecn}`:`${frame}`}
+          id="reading"
+          alt="product"
+        />
+        <div className="sec-2CT mx-auto pt-1">
+          {t("Intro")}
+          <br /> <br />
+          {t("Intro2")}
+          <br />
+        </div>
+        <div className="sec-2H2 mt-3">{t("CA")}</div>
         <img
           className="sec-3L mx-auto"
           src={older}
           id="reading"
-          alt="redaing"
+          alt="product"
         />
-        <div className="sec-2CT mx-auto pt-3">
-          {t("Intro")}
-          <br /> <br />
-          {t("Intro2")}
-        </div>
         <div className="sec-2E mx-auto mt-3 justify-content-center">
           <div className="sec-2C">
             <Button
@@ -69,6 +79,7 @@ const About = () => {
             <div className="sec-2CP">{t("BAText")}</div>
           </div>
         </div>
+
       </div>
     </div>
   );
