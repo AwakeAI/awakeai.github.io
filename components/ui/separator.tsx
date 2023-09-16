66b4ac880b3326c1b6850caa77d1c0ad63b0ddf5
linsh <lin.shilei@outlook.com>
2023-09-16 18:28:55 -0400

    Remove slug link from Team

--- components/ui/separator.tsx
+++ components/ui/separator.tsx
@@ -13,8 +13,7 @@ const Separator = React.forwardRef<
     { className, orientation = "horizontal", decorative = true, ...props },
     ref
   ) => (
-    <SeparatorPrimitive.Root
-      ref={ref}
+    <SeparatorPrimitive.Root      ref={ref}
       decorative={decorative}
       orientation={orientation}
       className={cn(
