9c3d1aff84b0a5a255d414ec47f96e86d9ba49da
linsh <lin.shilei@outlook.com>
2023-09-28 02:00:55 -0400

    Add Google Analytics

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,4 +1,5 @@
 import {Head, Html, Main, NextScript} from "next/document";
+import Script from "next/script";
 
 export default function Document() {
   return (
@@ -12,6 +13,16 @@ export default function Document() {
       <body>
         <Main />
         <NextScript />
+        <Script src="https://www.googletagmanager.com/gtag/js?id=G-JVXPVJ54JS" />
+        <Script id="google-analytics">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+          
+            gtag('config', 'G-JVXPVJ54JS');
+          `}
+        </Script>
       </body>
     </Html>
   );
