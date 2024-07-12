a88068c51e35debc6d93a478dccefaf8fa56a793
Shilei Lin <lin.shilei@outlook.com>
2024-07-12 16:34:56 +0800

    Remove i18n

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,8 +3,8 @@ import {Providers} from "@/app/provider";
 import {fonts} from "@/app/font";
 import {Header} from "@/components/Header";
 import {Footer} from "@/components/Footer"
-import {NextIntlClientProvider} from 'next-intl';
-import {getLocale, getMessages} from 'next-intl/server';
+// import {NextIntlClientProvider} from 'next-intl';
+// import {getLocale, getMessages} from 'next-intl/server';
 import {ColorModeScript} from '@chakra-ui/react'
 import {theme} from '@/app/theme';
 import Script from 'next/script'
@@ -12,23 +12,23 @@ import Script from 'next/script'
 export default async function RootLayout({children,}: Readonly<{
   children: React.ReactNode;
 }>) {
-  const locale = await getLocale();
+  // const locale = await getLocale();
 
   // Providing all messages to the client
   // side is the easiest way to get started
-  const messages = await getMessages();
+  // const messages = await getMessages();
 
   return (
-    <html lang={locale} className={fonts.inter.variable}>
+    <html lang='en' className={fonts.inter.variable}>
     <body>
     <ColorModeScript initialColorMode={theme.config.initialColorMode}/>
-    <NextIntlClientProvider messages={messages}>
-      <Providers>
-        <Header></Header>
-        {children}
-        <Footer></Footer>
-      </Providers>
-    </NextIntlClientProvider>
+    {/*<NextIntlClientProvider messages={messages}>*/}
+    <Providers>
+      <Header></Header>
+      {children}
+      <Footer></Footer>
+    </Providers>
+    {/*</NextIntlClientProvider>*/}
     </body>
     <Script src="https://www.googletagmanager.com/gtag/js?id=G-JVXPVJ54JS"/>
     <Script id="google-analytics">
